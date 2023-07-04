54ed7cd5104b4421148a9332c568c8730a3bd6ff
si-huynh-neo <huynh.neo@sharing-innovations.com>
2023-07-04 20:51:46 +0700

    First commit after init the controllerTest

---
+++ src/main/resources/insertData.sql
@@ -0,0 +1,4 @@
+insert into student(id,firstname,lastname,email_address) values (11,'Student', 'One', 'student.one@code_school.com')
+insert into student(id,firstname,lastname,email_address) values (12,'Student', 'Two', 'student.two@code_school.com')
+insert into student(id,firstname,lastname,email_address) values (13,'Student', 'Three', 'student.three@code_school.com')
+insert into student(id,firstname,lastname,email_address) values (14,'Student', 'Four', 'student.four@code)school.com')
